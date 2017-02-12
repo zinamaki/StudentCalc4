923126816f7303e8ed696f300a5afaa24474d6e1
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-02-12 12:02:25 -0500

    Added filter class

---
+++ src/filter/Throttle.java
@@ -0,0 +1,61 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class Throttle
+ */
+@WebFilter(
+		dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, 
+		urlPatterns = { "/Throttle" }, 
+		servletNames = { 
+				"Start", 
+				"Analytics"
+		})
+public class Throttle implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Throttle() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
