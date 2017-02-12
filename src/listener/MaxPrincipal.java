3fb9802245fb624fcb49278f4d5eb1c4ab19b547
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-02-12 11:53:20 -0500

    Refactor the max principal class and add comments

--- src/listener/MaxPrincipal.java
+++ src/listener/MaxPrincipal.java
@@ -17,65 +17,70 @@ public class MaxPrincipal implements ServletContextAttributeListener {
 	 * Default constructor.
 	 */
 	public MaxPrincipal() {
-		// TODO Auto-generated constructor stub
 		this.max_principal = 0;
 	}
 
 	/**
 	 * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+	 * 
+	 *      This method listens to when an attribute is added, and when it gets
+	 *      added it will check if the new value of principal is higher than the
+	 *      max, if so then update the max value
+	 * 
 	 */
-	public void attributeAdded(ServletContextAttributeEvent arg0) {
-		// TODO Auto-generated method stub
+	public void attributeAdded(ServletContextAttributeEvent attribute) {
 
-		//System.out.println("Attribute + " + arg0.getName() +"changed " + arg0.getValue());
-		
-		if(arg0.getName().equals("principal")){
-			try{
-				double principal = Double.parseDouble(arg0.getValue().toString());
-				
-				if(principal > max_principal){
+		if (attribute.getName().equals("principal")) {
+			try {
+				double principal = Double.parseDouble(attribute.getValue().toString());
+
+				if (principal > max_principal) {
 					this.max_principal = principal;
 					System.out.println("New max value");
 				}
-				
-			}catch(Exception e){
+
+			} catch (Exception e) {
 				System.out.println("Could not convert to double");
 			}
 		}
-		
-		
+
 	}
 
 	/**
 	 * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
 	 */
-	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+	public void attributeRemoved(ServletContextAttributeEvent attribute) {
 		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+	 * 
+	 *      This method listens to when an attribute is changed, and when it
+	 *      gets changed it will check if the new value of principal is higher
+	 *      than the max, if so then update the max value
+	 * 
 	 */
-	public void attributeReplaced(ServletContextAttributeEvent arg0) {
-		// TODO Auto-generated method stub
+	public void attributeReplaced(ServletContextAttributeEvent attribute) {
 
-		System.out.println("Attribute replaced " + arg0.getValue());
+		if (attribute.getName().equals("principal")) {
+			try {
+				double principal = Double.parseDouble(attribute.getValue().toString());
 
-		if(arg0.getName().equals("principal")){
-			try{
-				double principal = Double.parseDouble(arg0.getValue().toString());
-				
-				if(principal > max_principal){
+				if (principal > max_principal) {
 					this.max_principal = principal;
 					System.out.println("New max value");
 				}
-				
-			}catch(Exception e){
+
+			} catch (Exception e) {
 				System.out.println("Could not convert to double");
 			}
 		}
 	}
 
+	/**
+	 * Accessor for the max_principal variable
+	 */
 	public static double getMaxPrincipal() {
 		return max_principal;
 	}
