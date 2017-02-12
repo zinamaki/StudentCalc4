7be0dbba5a72783b0f4b1135a5a334451623acf6
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-02-12 11:15:07 -0500

    Added listener

---
+++ src/listener/MaxPrincipal.java
@@ -0,0 +1,42 @@
+package listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class MaxPrincipal
+ *
+ */
+@WebListener
+public class MaxPrincipal implements ServletContextAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MaxPrincipal() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
