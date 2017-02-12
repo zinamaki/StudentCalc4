bc3337c1bd610428f2c20eecfd1f9c8156bab995
Mina Zaki <minazaki@Minas-MacBook-Pro.local>
2017-02-12 11:40:24 -0500

    Keep track of largest input principal value in the listener

--- src/listener/MaxPrincipal.java
+++ src/listener/MaxPrincipal.java
@@ -11,32 +11,73 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class MaxPrincipal implements ServletContextAttributeListener {
 
-    /**
-     * Default constructor. 
-     */
-    public MaxPrincipal() {
-        // TODO Auto-generated constructor stub
-    }
+	private double max_principal;
 
 	/**
-     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
-     */
-    public void attributeAdded(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
+	 * Default constructor.
+	 */
+	public MaxPrincipal() {
+		// TODO Auto-generated constructor stub
+		this.max_principal = 0;
+	}
 
 	/**
-     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
-     */
-    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
+	 * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+	 */
+	public void attributeAdded(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+
+		//System.out.println("Attribute + " + arg0.getName() +"changed " + arg0.getValue());
+		
+		if(arg0.getName().equals("principal")){
+			try{
+				double principal = Double.parseDouble(arg0.getValue().toString());
+				
+				if(principal > max_principal){
+					this.max_principal = principal;
+					System.out.println("New max value");
+				}
+				
+			}catch(Exception e){
+				System.out.println("Could not convert to double");
+			}
+		}
+		
+		
+	}
 
 	/**
-     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
-     */
-    public void attributeReplaced(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-	
+	 * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+	 */
+	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+	 */
+	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Attribute replaced " + arg0.getValue());
+
+		if(arg0.getName().equals("principal")){
+			try{
+				double principal = Double.parseDouble(arg0.getValue().toString());
+				
+				if(principal > max_principal){
+					this.max_principal = principal;
+					System.out.println("New max value");
+				}
+				
+			}catch(Exception e){
+				System.out.println("Could not convert to double");
+			}
+		}
+	}
+
+	public double getMaxPrincipal() {
+		return this.max_principal;
+	}
+
 }
